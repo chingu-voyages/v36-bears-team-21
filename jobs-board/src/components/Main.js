ba995891f9048f7a1f2b827939d22d818c799635
maknetaRo <makneta@gmail.com>
2022-02-09 17:01:58 +0100

    add responsiveness to list view

--- jobs-board/src/components/Main.js
+++ jobs-board/src/components/Main.js
@@ -3,7 +3,7 @@ import ListView from './ListView';
 
 const Main = () => {
   return (
-    <main className="max-w-screen-2xl  m-auto">
+    <main className="max-w-screen-2xl  mx-auto my-24">
       <ListView />
     </main>
   );
